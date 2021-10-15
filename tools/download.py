3536215a4599deb14c15a300b6175d1950c63686
felixmusil <felix.musil@epfl.ch>
2021-10-15 18:20:18 +0200

    add download

---
+++ tools/download.py
@@ -0,0 +1,62 @@
+import urllib.request
+import requests
+import sys
+import os
+import os.path as osp
+import errno
+
+from .utils import tqdm
+
+
+def makedirs(path):
+    try:
+        os.makedirs(osp.expanduser(osp.normpath(path)))
+    except OSError as e:
+        if e.errno != errno.EEXIST and osp.isdir(path):
+            raise e
+
+class DownloadProgressBar(tqdm):
+    def update_to(self, b=1, bsize=1, tsize=None):
+        if tsize is not None:
+            self.total = tsize
+        self.update(b * bsize - self.n)
+
+
+def download_url(url: str, folder: str, log: bool = True):
+    r"""Downloads the content of an URL to a specific folder.
+
+    Args:
+        url (string): The url.
+        folder (string): The folder.
+        log (bool, optional): If :obj:`False`, will not print anything to the
+            console. (default: :obj:`True`)
+
+    Adtapted from torch_geometric.data.download.py
+    """
+    import os.path as osp
+
+    filename = url.rpartition("/")[2].split("?")[0]
+    path = osp.join(folder, filename)
+
+    if osp.exists(path):  # pragma: no cover
+        if log:
+            print(f"Using existing file {filename}", file=sys.stderr)
+        return path
+
+    if log:
+        print(f"Downloading {url}", file=sys.stderr)
+
+    makedirs(folder)
+
+    response = requests.head(url)
+    total_size = int(response.headers["content-length"])
+    with DownloadProgressBar(
+        unit="B",
+        total=total_size,
+        unit_scale=True,
+        miniters=1,
+        desc=url.split("/")[-1],
+    ) as t:
+        urllib.request.urlretrieve(url, filename=path, reporthook=t.update_to)
+
+    return path
